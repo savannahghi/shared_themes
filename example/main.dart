5cc8b6708bf853bdf4391ac55b4d5dffc5f07c1f
abdi-adan <adanabdi036@gmail.com>
2021-06-11 13:10:12 +0300

    infra: publish to pub.dev

---
+++ example/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:shared_themes/colors.dart';
+import 'package:shared_themes/spaces.dart';
+
+void main() {
+  runApp(const SharedThemesExample());
+}
+
+class SharedThemesExample extends StatefulWidget {
+  /// This widget is the root of your application.
+  const SharedThemesExample({Key? key}) : super(key: key);
+
+  @override
+  _SharedThemesExampleState createState() => _SharedThemesExampleState();
+}
+
+class _SharedThemesExampleState extends State<SharedThemesExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Debug Logger example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+
+            /// Uses the sizes exported as doubles in `spaces.dart`
+            const SizedBox(height: Sizing.size4),
+            Text(
+              'Counter value',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        /// Uses the color constants exported as colors in `colors.dart`
+        backgroundColor: red,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
